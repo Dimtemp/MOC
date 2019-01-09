be690b62d1864ac3aa9a0a873f5f6979b9eea9ac
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2019-01-09 22:43:53 +0100

    Create 20745-Lab06-prep.ps1

---
+++ 20745-Lab06-prep.ps1
@@ -0,0 +1,15 @@
+$UserName = 'Adatum\Administrator'
+$LocalUserName = '.\Administrator'
+$SecurePassword = (ConvertTo-Securestring –asplaintext –force 'Pa55w.rd')
+$Credential1 = New-Object -TypeName System.Management.Automation.PSCredential  -ArgumentList $UserName,$SecurePassword
+$Credential2 = New-Object -TypeName System.Management.Automation.PSCredential  -ArgumentList $LocalUserName,$SecurePassword
+New-ADGroup -Name 'VMM-NC-Mgmt' -GroupScope DomainLocal -Path 'OU=IT,DC=Adatum,DC=com'
+Add-ADGroupMember -Identity 'VMM-NC-Mgmt' -Members 'Domain Admins'
+New-ADGroup -Name 'VMM-NC-Clients' -GroupScope DomainLocal -Path 'OU=IT,DC=Adatum,DC=com'
+New-ADUser -AccountPassword (ConvertTo-SecureString 'Pa55w.ord' -AsPlainText -Force) -ChangePasswordAtLogon $false -Enabled $true -Name 's-VMM-NC-Client' -Path 'OU=IT,DC=Adatum,DC=com'
+Add-ADGroupMember -Identity 'VMM-NC-Clients' -Members 's-VMM-NC-Client'
+New-SCRunAsAccount -Name 'ADATUM\s-VMM-NC-Client' -Credential $Credential1
+New-SCRunAsAccount -Name $LocalUserName -Credential $Credential2 -NoValidation
+New-SelfSignedCertificate -KeyUsageProperty All -Provider 'Microsoft Strong Cryptographic Provider' -FriendlyName 'AdatumNC' -DnsName @('NC-VM01.adatum.com')
+mkdir \\LON-SVR3\VMMLibrary\NC\NCCertificate.cr
+mkdir \\LON-SVR3\VMMLibrary\NC\TrustedRootCertificate.cr
