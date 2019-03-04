ef339e99c77c8252e0a699009096513fbd58b5fd
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2019-03-04 23:10:15 +0100

    Update AZ-100-T03-create-VM.ps1

--- AZ-100-T03-create-VM.ps1
+++ AZ-100-T03-create-VM.ps1
@@ -1,25 +1,37 @@
+# init
 $vmName = 'az1000301-vm1'
 $vmSize = 'Standard_DS1_v2'
+
+# get basic details from Azure
 $resourceGroup = Get-AzResourceGroup -Name 'az1000301-RG'
 $location = $resourceGroup.Location
+
+# get availability set
 $availabilitySet = Get-AzAvailabilitySet -ResourceGroupName $resourceGroup.ResourceGroupName -Name 'az1000301-avset0'
+
+# prepare networking
 $vnet = Get-AzVirtualNetwork -Name 'az1000301-vnet0' -ResourceGroupName $resourceGroup.ResourceGroupName
 $subnetid = (Get-AzVirtualNetworkSubnetConfig -Name 'subnet0' -VirtualNetwork $vnet).Id
 $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup.ResourceGroupName -Location $location -Name "$vmName-nsg"
 $pip = New-AzPublicIpAddress -Name "$vmName-ip" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $location -AllocationMethod Dynamic 
 $nic = New-AzNetworkInterface -Name "$($vmName)$(Get-Random)" -ResourceGroupName $resourceGroup.ResourceGroupName -Location $location -SubnetId $subnetid -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# credentials
 $adminUsername = 'Student'
 $adminPassword = 'Pa55w.rd1234'
 $adminCreds = New-Object PSCredential $adminUsername, ($adminPassword | ConvertTo-SecureString -AsPlainText -Force)
+
+# operating system
 $publisherName = 'MicrosoftWindowsServer'
 $offerName = 'WindowsServer'
 $skuName = '2016-Datacenter'
 $osDiskType = (Get-AzResource -ResourceGroupName $resourceGroup.ResourceGroupName -ResourceType Microsoft.Compute/disks)[0].Sku.name
 $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize -AvailabilitySetId $availabilitySet.Id
+
+# create VM
 Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
 Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $adminCreds 
 Set-AzVMSourceImage -VM $vmConfig -PublisherName $publisherName -Offer $offerName -Skus $skuName -Version 'latest'
 Set-AzVMOSDisk -VM $vmConfig -Name "$($vmName)_OsDisk_1_$(Get-Random)" -StorageAccountType $osDiskType -CreateOption fromImage
 Set-AzVMBootDiagnostics -VM $vmConfig -Disable
 New-AzVM -ResourceGroupName $resourceGroup.ResourceGroupName -Location $location -VM $vmConfig
-
