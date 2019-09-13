c810287377f6631f762e4f90843db48b869c62fa
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2019-09-13 14:00:41 +0200

    Update remove-azresourcegroup.ps1

--- remove-azresourcegroup.ps1
+++ remove-azresourcegroup.ps1
@@ -1,11 +1,7 @@
 # This script will remove selected resource groups from your subscription
-
-
 # Use Windows PowerShell or PowerShell core. This will not work from the cloud shell.
 
 Get-AzResourceGroup |
 Out-GridView -Title 'select resource groups to remove' -OutputMode Multiple |
 Remove-AzResourceGroup -force
-
-# Get-AzResourceGroup | ogv -output Multiple | foreach { Remove-AzResourceGroup -Name $_.ResourceGroupName -force }
-
+# foreach { Remove-AzResourceGroup -Name $_.ResourceGroupName -force }
