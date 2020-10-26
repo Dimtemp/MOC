518316044be1a420b8ddb5786f3db4e04d7df116
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-10-26 21:30:49 +0100

    Update DP-300-M03-ownership-chaining.sql

--- DP-300-M03-ownership-chaining.sql
+++ DP-300-M03-ownership-chaining.sql
@@ -1,8 +1,11 @@
 -- prereq: using SSMS restore database from:
 -- https://github.com/MicrosoftLearning/DP-300T00-Administering-Relational-Databases-on-Azure/blob/master/Allfiles/Labs/Secure%20Environment/AdventureWorks.bacpac
+
+
 -- create a user in the database
 CREATE USER [User1] WITH PASSWORD = 'Azur3Pa$$'
 GO
+
 
 -- create a custom role and add the user to it
 CREATE ROLE [SalesReader]
@@ -10,9 +13,11 @@ GO
 ALTER ROLE [SalesReader] ADD MEMBER [User1]
 GO
 
+
 -- grant permissions to the role
 GRANT SELECT, EXECUTE ON SCHEMA::Sales TO [SalesReader]
 GO
+
 
 -- create a new stored procedure in the Sales schema, accesses a table in the Product schema
 CREATE OR ALTER PROCEDURE Sales.DemoProc
@@ -24,6 +29,7 @@ INNER JOIN Sales.SalesOrderHeader SOH on SOH.SalesOrderID = SOD.SalesOrderID
 GROUP BY P.Name, SOH.OrderDate
 ORDER BY TotalSales DESC
 GO
+
 
 -- test out the security you just created
 EXECUTE AS USER = 'User1'
@@ -37,6 +43,7 @@ ORDER BY TotalSales DESC
 
 -- query will fail: SELECT permission was denied on the Production.Product table
 -- role has SELECT permission in the Sales schema, but not in the Production schema
+
 
 -- ownership chaining: provide data access to users who do not have direct permissions to access database objects. 
 -- For all objects that have the same owner, the database engine only checks the EXECUTE permission on the procedure and not the underlying objects
