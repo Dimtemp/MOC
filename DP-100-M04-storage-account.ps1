56f7968b91662ff9c1258f03edbb84624af0d224
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-11-10 13:14:15 +0100

    Update DP-100-M04-storage-account.ps1

--- DP-100-M04-storage-account.ps1
+++ DP-100-M04-storage-account.ps1
@@ -3,17 +3,18 @@
 # new storage account (LRS)
 $resourceGroup = 'ML-M04'
 $location = 'westeurope'
-$accountName = "ML04$(Get-Random)"
+$accountName = "m04$(Get-Random)"
 $SkuName = 'Standard_LRS'
 $containerName = 'aml-data'
 
 New-AzResourceGroup -Name $resourceGroup -Location $location
 
 $storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup -Name $accountName -Location $location -SkuName $SkuName -Kind StorageV2
-
 $ctx = $storageAccount.Context
 
 New-AzStorageContainer -Name $containerName -Context $ctx -Permission blob
 
 # Access keys: copy key
-Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name x
+$key = Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $accountName
+$key[0].Value | Clip
+Write-Host 'The storage account key is available in your clipboard'
