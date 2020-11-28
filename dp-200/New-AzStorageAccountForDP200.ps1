70de4e548c0c77c23ca5595e9fe73789e2706e6e
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-11-28 23:13:29 +0100

    Update New-AzStorageAccountForDP200.ps1

--- dp-200/New-AzStorageAccountForDP200.ps1
+++ dp-200/New-AzStorageAccountForDP200.ps1
@@ -1,38 +1,41 @@
-<#
-.SYNOPSIS
-This script creates prerequisite file for module 3 and later from the DP-200 training.
+function New-AzStorageAccountForDP200Training {
+    <#
+    .SYNOPSIS
+    Creates prerequisite file for module 3 and later from the DP-200 training.
+    .NOTES
+    Containers & files:
+    \data\DimDate2.txt      required for Module 5: SQL and Synapse (DW)
+    \logs\preferences.json  required for Module 3: Databricks
+    \phonecalls             required for Module 6: stream analytics
+    #>
 
-.NOTES
-Containers & files:
-\data\DimDate2.txt      required for Module 5: SQL and Synapse (DW)
-\logs\preferences.json  required for Module 3: Databricks
-\phonecalls             required for Module 6: stream analytics
-#>
+    param(
+        $rgName = 'dp200',
+        $location = 'westeurope',
+        $saName = "dp200x$(Get-Random -Maximum 1000)"
+    )
 
-param(
-    $rgName = 'dp200',
-    $location = 'westeurope',
-    $saName = "dp200x$(Get-Random -Maximum 1000)"
-)
+    $containers = 'data', 'logs', 'phonecalls'
+    $file1 = 'https://raw.githubusercontent.com/MicrosoftLearning/DP-200-Implementing-an-Azure-Data-Solution/master/Labfiles/Starter/DP-200.2/Static%20files/DimDate2.txt'
+    $file2 = 'https://raw.githubusercontent.com/MicrosoftLearning/DP-200-Implementing-an-Azure-Data-Solution/master/Labfiles/Starter/DP-200.2/logs/preferences.json'
 
-$containers = 'data', 'logs', 'phonecalls'
-$file1 = 'https://raw.githubusercontent.com/MicrosoftLearning/DP-200-Implementing-an-Azure-Data-Solution/master/Labfiles/Starter/DP-200.2/Static%20files/DimDate2.txt'
-$file2 = 'https://raw.githubusercontent.com/MicrosoftLearning/DP-200-Implementing-an-Azure-Data-Solution/master/Labfiles/Starter/DP-200.2/logs/preferences.json'
+    New-AzResourceGroup -Location $location -Name $rgName
 
-New-AzResourceGroup -Location $location -Name $rgName
+    # Check StorageAccountNameAvailability
+    $saNameAvailable = Get-AzStorageAccountNameAvailability -Name $saName
 
-# Check StorageAccountNameAvailability
-$saNameAvailable = Get-AzStorageAccountNameAvailability -Name $saName
+    if ($saNameAvailable.NameAvailable)
+    {
+        # Create storage account, from lab: Std/V2/RA_GRS/Hot/HIERARCHICAL
+        $s = New-AzStorageAccount -ResourceGroupName $rgName -Name $saName -SkuName Standard_LRS -Location $location -EnableHierarchicalNamespace $true
 
-if ($saNameAvailable.NameAvailable)
-{
-    # Create storage account, from lab: Std/V2/RA_GRS/Hot/HIERARCHICAL
-    $s = New-AzStorageAccount -ResourceGroupName $rgName -Name $saName -SkuName Standard_LRS -Location $location -EnableHierarchicalNamespace $true
+        # create containers
+        $containers | ForEach-Object { New-AzStorageContainer -Name $_ -Context $s.Context }
 
-    # create containers
-    $containers | ForEach-Object { New-AzStorageContainer -Name $_ -Context $s.Context }
-
-    # create files
-    Start-AzStorageBlobCopy -DestContext $s.Context -DestContainer 'data' -DestBlob (Split-Path $file1 -Leaf) -AbsoluteUri $file1
-    Start-AzStorageBlobCopy -DestContext $s.Context -DestContainer 'logs' -DestBlob (Split-Path $file2 -Leaf) -AbsoluteUri $file2
+        # create files
+        Start-AzStorageBlobCopy -DestContext $s.Context -DestContainer 'data' -DestBlob (Split-Path $file1 -Leaf) -AbsoluteUri $file1
+        Start-AzStorageBlobCopy -DestContext $s.Context -DestContainer 'logs' -DestBlob (Split-Path $file2 -Leaf) -AbsoluteUri $file2
+    }
 }
+
+New-AzStorageAccountForDP200Training
