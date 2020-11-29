8da687e75c56d1ae0ad7dfc3a5a510c1d00ea44e
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-11-29 13:29:28 +0100

    Create DP-200-M02-BlobQuery.ps1

---
+++ dp-200/DP-200-M02-BlobQuery.ps1
@@ -0,0 +1,27 @@
+Get-AzProviderFeature -ProviderNamespace Microsoft.Storage -FeatureName BlobQuery
+# optionally
+Register-AzProviderFeature -ProviderNamespace Microsoft.Storage -FeatureName BlobQuery
+
+$t = New-TemporaryFile
+
+$inputconfig = New-AzStorageBlobQueryConfig -AsJson -RecordSeparator "`n"
+
+$ctx = New-AzStorageContext -StorageAccountName "dp200x929" -StorageAccountKey "hXgQU1iilx9MkdCrtJZEyLu9ts0NfO8ea46lCkE7s4MOFXNV09w7bA0xid2IsACuhhPavnYR19eeYixUKH8o7w==" # -Anonymous
+
+$HashArguments = @{
+    Container = 'logs'
+    Blob = 'preferences.json'
+    QueryString = 'SELECT * FROM BlobStorage'
+    InputTextConfiguration = $inputconfig
+    ResultFile = $t
+    Context = $ctx
+}
+
+Get-AzStorageBlobQueryResult @HashArguments
+# -OutputTextConfiguration (New-AzStorageBlobQueryConfig -AsCsv -HasHeader) -ResultFile $tempfile.FullName -Force
+
+
+<#
+More information
+https://docs.microsoft.com/en-us/azure/storage/blobs/data-lake-storage-query-acceleration-how-to
+#>
