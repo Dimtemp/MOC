4075800c17cc29269705d84df53e6855c731826f
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2020-11-29 13:30:53 +0100

    Update DP-200-M02-BlobQuery.ps1

--- dp-200/DP-200-M02-BlobQuery.ps1
+++ dp-200/DP-200-M02-BlobQuery.ps1
@@ -1,12 +1,16 @@
+
+
 Get-AzProviderFeature -ProviderNamespace Microsoft.Storage -FeatureName BlobQuery
 # optionally
 Register-AzProviderFeature -ProviderNamespace Microsoft.Storage -FeatureName BlobQuery
+
+$Key1 = "hXgQU1iilx9MkdCrtJZEyLu9ts0NfO8ea46lCkE7s4MOFXNV09w7bA0xid2IsACuhhPavnYR19eeYixUKH8o7w=="
 
 $t = New-TemporaryFile
 
 $inputconfig = New-AzStorageBlobQueryConfig -AsJson -RecordSeparator "`n"
 
-$ctx = New-AzStorageContext -StorageAccountName "dp200x929" -StorageAccountKey "hXgQU1iilx9MkdCrtJZEyLu9ts0NfO8ea46lCkE7s4MOFXNV09w7bA0xid2IsACuhhPavnYR19eeYixUKH8o7w==" # -Anonymous
+$ctx = New-AzStorageContext -StorageAccountName 'dp200x929' -StorageAccountKey $Key1  # -Anonymous
 
 $HashArguments = @{
     Container = 'logs'
