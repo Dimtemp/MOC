0916e28d6057ec004c3f6a8efaaf0f42080bc346
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2021-09-16 23:42:30 +0200

    Create 55202-DSC-SeparateDataAndEnv.ps1

---
+++ 55202-DSC-SeparateDataAndEnv.ps1
@@ -0,0 +1,41 @@
+# display the hostname
+Hostname
+
+Configuration MyDscConfiguration
+{
+    Node $AllNodes.Where{$_.Role -eq "WebServer"}.NodeName
+    {
+        WindowsFeature IISInstall
+        {
+            Ensure = 'Present'
+            Name   = 'Web-Server'
+        }
+    }
+    
+    Node $AllNodes.Where{$_.Role -eq "VMHost"}.NodeName
+    {
+        WindowsFeature HyperVInstall {
+            Ensure = 'Present'
+            Name   = 'Hyper-V'
+        }
+    }
+}
+
+$MyData =
+@{
+    AllNodes =
+    @(
+        @{
+            NodeName    = 'VM-1'   # REPLACE WITH HOSTNAME
+            Role = 'WebServer'
+        },
+
+        @{
+            NodeName    = 'VM-2'
+            Role = 'VMHost'
+        }
+    )
+}
+
+MyDscConfiguration -ConfigurationData $MyData -Output C:\DataTest
+Run-DscConfiguration C:\DataTest
