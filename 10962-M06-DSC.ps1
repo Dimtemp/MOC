883df06250f0263baabadecbfc32f872ead58ca5
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2021-09-17 00:01:56 +0200

    Update 10962-M06-DSC.ps1

--- 10962-M06-DSC.ps1
+++ 10962-M06-DSC.ps1
@@ -1,39 +1,61 @@
-Configuration SecondTestConfig {
+# enable DSC log
+wevtutil.exe set-log "Microsoft-Windows-Dsc/Analytic" /q:true /e:true
+
+# inspect service
+Get-WmiObject win32_service | where name -match wu
+
+# inspect telnet client
+Get-WindowsFeature t*
+
+
+
+Configuration DscResourceTest {
+    Import-DscResource –ModuleName  'PSDesiredStateConfiguration'
       Node localhost {
 
         Group GroupExample
         {
             # This will create TestGroup
-            Ensure = "Present"
-             GroupName = "TestGroup"
-             Description = 'dsc test group'
+            Ensure = 'Present'
+             GroupName = 'TestGroup'
+             Description = 'DSC test group'
         }
 
         Log LogExample
         {
-            Message = "message for Microsoft-Windows-Desired State Configuration/Analytic event log"
+            Message = 'message for Microsoft-Windows-Desired State Configuration/Analytic event log'
         }
 
         Registry RegistryExample
         {
-            Ensure = "Present"  # You can also set Ensure to "Absent"
-            Key = "HKEY_LOCAL_MACHINE\SOFTWARE\ExampleKey"
-            ValueName = "TestValue"
-            ValueData = "TestData"
+            Ensure = 'Present'
+            Key = 'HKEY_LOCAL_MACHINE\SOFTWARE\ExampleKey'
+            ValueName = 'TestValue'
+            ValueData = 123
         }
 
         WindowsFeature RoleExample
         {
-            Ensure = "Present" # Alternatively, to ensure the role is uninstalled, set Ensure to "Absent"
-            Name = "telnet-client" # Use the Name property from Get-WindowsFeature 
+            Ensure = 'Present'
+            Name = 'telnet-client'
         }
 
         Service ServiceExample
         {
-            Name = "wuauserv"
-            StartupType = "Manual"
-            State = "Running"
+            Name = 'wuauserv'
+            StartupType = 'Manual'
+            State = 'Running'
         }
 
      }   # localhost
 }   # SecondTestConfig
+
+# run it
+DscResourceTest -output c:\DscResourceTest
+Start-DscConfiguration c:\DscResourceTest -Wait -Verbose
+
+# inspect
+regedit
+Get-WmiObject win32_service | where name -match wu
+Get-WindowsFeature t*
+EventVwr
