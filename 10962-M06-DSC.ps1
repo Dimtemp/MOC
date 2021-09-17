f83fb3cdb70829c72c4455ac3dd674bbbee6d775
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2021-09-17 13:13:08 +0200

    Update 10962-M06-DSC.ps1

--- 10962-M06-DSC.ps1
+++ 10962-M06-DSC.ps1
@@ -1,5 +1,6 @@
 # enable DSC log
 wevtutil.exe set-log "Microsoft-Windows-Dsc/Analytic" /q:true /e:true
+# don't forget to enable Debug and Analytical logs in EventVwr
 
 # inspect service
 Get-WmiObject win32_service | where name -match wu
@@ -16,17 +17,48 @@ Configuration DscResourceTest {
     Import-DscResource –ModuleName  'PSDesiredStateConfiguration'
       Node localhost {
 
+        Archive ArchiveExample {
+            Ensure = "Present"
+            Path = "C:\Users\Public\Documents\Test.zip"
+            Destination = "C:\Users\Public\Documents\ExtractionPath"
+        }
+
+        Environment EnvironmentExample
+        {
+            Ensure = "Present"  # You can also set Ensure to "Absent"
+            Name = "DSCTestEnvironmentVariable"
+            Value = "TestValue"
+        }
+
+        File DirectoryCopy
+        {
+             Ensure = "Present"
+             Type = "Directory" # Default is "File".
+             Recurse = $true
+             SourcePath = "C:\Users\Public\Documents\DSCDemo\DemoSource"
+             DestinationPath = "C:\Users\Public\Documents\DSCDemo\DemoDestination"   
+        }
+    
         Group GroupExample
         {
-            # This will create TestGroup
             Ensure = 'Present'
              GroupName = 'TestGroup'
              Description = 'DSC test group'
-        }
+            #members
+            #MembersToInclude
+         }
 
         Log LogExample
         {
             Message = 'message for Microsoft-Windows-Desired State Configuration/Analytic event log'
+        }
+
+        Package PackageExample
+        {
+            Ensure = "Present"  # You can also set Ensure to "Absent"
+            Path  = "$Env:SystemDrive\TestFolder\TestProject.msi"
+            Name = "TestPackage"
+            ProductId = "ACDDCDAF-80C6-41E6-A1B9-8ABD8A05027E"
         }
 
         Registry RegistryExample
@@ -37,10 +69,16 @@ Configuration DscResourceTest {
             ValueData = 123
         }
 
-        WindowsFeature RoleExample
+
+        Script ScriptExample
         {
-            Ensure = 'Present'
-            Name = 'telnet-client'
+            SetScript = {
+                $sw = New-Object System.IO.StreamWriter("C:\TempFolder\TestFile.txt")
+                $sw.WriteLine("Some sample string")
+                $sw.Close()
+            }
+            TestScript = { Test-Path "C:\TempFolder\TestFile.txt" }
+            GetScript = { <# This must return a hash table #> }         
         }
 
         Service ServiceExample
@@ -50,7 +88,30 @@ Configuration DscResourceTest {
             State = 'Running'
         }
 
-     }   # localhost
+        User UserExample
+        {
+            Ensure = "Present"
+            UserName = "UserNameExample"
+            #Password = $passwordCred
+            DependsOn = “[Group]GroupExample"
+        }
+
+
+        WindowsFeature RoleExample
+        {
+            Ensure = 'Present'
+            Name = 'telnet-client'
+        }
+
+        WindowsProcess [string] #ResourceName
+        {
+            Arguments = '-d'
+            Path = notepad.exe
+            Ensure = Present
+            WorkingDirectory = "c:\"
+        }
+
+}   # localhost
 }   # SecondTestConfig
 
 # run it
