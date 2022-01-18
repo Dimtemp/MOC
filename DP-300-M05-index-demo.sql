39ba151c175e295664f98655a059deeb703786ff
Dimitri Koens <40596624+Dimtemp@users.noreply.github.com>
2022-01-18 16:32:55 +0100

    Update DP-300-M05-index-demo.sql

--- DP-300-M05-index-demo.sql
+++ DP-300-M05-index-demo.sql
@@ -3,7 +3,7 @@ CREATE TABLE dbo.PhoneLog
 ( PhoneLogID int IDENTITY(1,1) PRIMARY KEY,
   LogRecorded datetime2 NOT NULL,
   PhoneNumberCalled nvarchar(100) NOT NULL,
-  CallDurationMs int NOT NULL
+  CallDurationSec int NOT NULL
 );
 GO
 
@@ -11,7 +11,7 @@ GO
 SET NOCOUNT ON;
 DECLARE @Counter int = 0;
 WHILE @Counter < 10000 BEGIN
-  INSERT dbo.PhoneLog (LogRecorded, PhoneNumberCalled, CallDurationMs)
+  INSERT dbo.PhoneLog (LogRecorded, PhoneNumberCalled, CallDurationSec)
     VALUES(SYSDATETIME(),'999-9999',CAST(RAND() * 1000 AS int));
   SET @Counter += 1;
 END;
@@ -22,18 +22,17 @@ GO
 -- Retrieve the data and inspect the execution plan
 -- You might want to change the value to retrieve actual results
 SELECT * FROM dbo.PhoneLog
-WHERE CallDurationMs = 100
+WHERE CallDurationSec = 991
 
 -- Create an index
-CREATE NONCLUSTERED INDEX MyIndex ON dbo.PhoneLog ( CallDurationMs ASC )
+CREATE NONCLUSTERED INDEX MyIndex ON dbo.PhoneLog ( CallDurationSec ASC )
 
 -- Retrieve the data and inspect the execution plan
 SELECT * FROM dbo.PhoneLog
-WHERE CallDurationMs = 100
+WHERE CallDurationSec = 991
 
 -- Drop the index
 DROP INDEX MyIndex ON dbo.PhoneLog
 
 -- Drop the table
 DROP TABLE dbo.PhoneLog
-
