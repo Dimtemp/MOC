969d754f877a1af0e5c2a4bf98c8db4ebb1e648d
Dimitri Koens <dimitrikoens@gmail.com>
2026-06-01 23:30:48 +0200

    added data masking script

---
+++ DP-300-M02-data-masking.sql
@@ -0,0 +1,34 @@
+-- prereq: AdventureWorksLT database
+
+SELECT TOP 10 FirstName, MiddleName, LastName
+FROM SalesLT.Customer;
+
+-- Create a new SQL user and give them a password
+CREATE USER Bob WITH PASSWORD = 'c0mpl3xPassword!';
+
+-- Until you run the following two lines, Bob has no access to read or write data
+ALTER ROLE db_datareader ADD MEMBER Bob;
+ALTER ROLE db_datawriter ADD MEMBER Bob;
+
+-- Execute as our new, low-privilege user, Bob
+EXECUTE AS USER = 'Bob';
+SELECT TOP 10 FirstName, MiddleName, LastName
+FROM SalesLT.Customer;
+REVERT;
+
+GRANT UNMASK TO Bob;  
+EXECUTE AS USER = 'Bob';
+SELECT TOP 10 FirstName, MiddleName, LastName
+FROM SalesLT.Customer;
+REVERT;  
+
+
+-- Remove unmasking privilege
+REVOKE UNMASK TO Bob;  
+
+-- Execute as Bob
+EXECUTE AS USER = 'Bob';
+SELECT TOP 10 FirstName, MiddleName, LastName
+FROM SalesLT.Customer;
+REVERT;  
+
